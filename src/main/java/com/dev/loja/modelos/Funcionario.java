0723683bbf4925030f49b41ad6b42004ab09eb36
CristianCeccon <cristian_ceccon45@hotmail.com>
2021-08-25 08:52:39 -0300

    comit final

--- src/main/java/com/dev/loja/modelos/Funcionario.java
+++ src/main/java/com/dev/loja/modelos/Funcionario.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -29,7 +30,8 @@ public class Funcionario implements Serializable{
 	@Temporal(TemporalType.DATE)
 	private Date dataSaida;
 	private String cargo;
-	private String cidade;
+	@ManyToOne
+	private Cidade cidade;
 	private String logradouro;
 	private String numero;
 	private String complemento;
@@ -72,10 +74,10 @@ public class Funcionario implements Serializable{
 	public void setCargo(String cargo) {
 		this.cargo = cargo;
 	}
-	public String getCidade() {
+	public Cidade getCidade() {
 		return cidade;
 	}
-	public void setCidade(String cidade) {
+	public void setCidade(Cidade cidade) {
 		this.cidade = cidade;
 	}
 	public String getLogradouro() {
